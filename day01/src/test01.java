6f1185ce9182c14ca208b1c37e4248e6c14f6bd2
曾庆龙 <cengqinglong@cengqinglongdeMacBook-Air.local>
2018-09-11 17:39:45 +0800

    complete test 2

--- day01/src/test01.java
+++ day01/src/test01.java
@@ -1,4 +1,10 @@
+import com.sun.jdi.event.ThreadStartEvent;
+
+import java.lang.reflect.Constructor;
 import java.util.ArrayList;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /*
 给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
@@ -9,11 +15,16 @@ import java.util.ArrayList;
   所以需要两个for完成
  */
 public class test01 {
-    public static void main(String[] args) {
-        int arr[] = {1,2,7,8,11,15};
-        int s = 9;
-        ArrayList<String> show = show(arr, s);
-        System.out.println(show);
+    public static void main(String[] args) throws  IllegalArgumentException {
+        ExecutorService service = Executors.newFixedThreadPool(2);
+        int arr[] = {1,2,7,11,15,101};
+        int s = 108;
+        //ArrayList<String> show = show(arr, s);
+        //System.out.println(show);
+        int[] ints = twoSum(arr, s);
+        for (int anInt : ints) {
+            System.out.println(anInt);
+        }
     }
 
     private static ArrayList<String> show(int[] arr, int s) {
@@ -28,5 +39,16 @@ public class test01 {
         }
         return list;
     }
-
+    public static int[] twoSum(int[] nums, int target) {
+        int [] arr = new int[2];
+        for(int i = 0 ; i < nums.length ; i++){
+            for(int j = i+1; j < nums.length ; j++){
+                if(nums[i]+nums[j] == target){
+                    arr[0]=i;
+                    arr[1]=j;
+                }
+            }
+        }
+        return arr;
+    }
 }
