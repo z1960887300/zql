6f8e570bdb43de5c828f328eb0a7e6eb88fe3277
曾庆龙 <cengqinglong@cengqinglongdeMacBook-Air.local>
2018-09-13 20:40:40 +0800

    aaa

--- day01/src/test04.java
+++ day01/src/test04.java
@@ -1,2 +1,28 @@
+/*
+判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+ */
 public class test04 {
+    public static void main(String[] args) {
+        boolean show = show(-121);
+        System.out.println("是否是回文:"+show);
+    }
+
+    private static boolean show(int i) {
+        if(i < 0){
+            return false;
+        }else {
+        Integer s1 = i;
+        int s = 0;
+       while (s1 != 0){
+           int i1 = s1% 10;
+           s1 = s1 / 10;
+           s = s*10+i1;
+       }
+       if(s == i){
+           return  true;
+       }else {
+           return  false;
+       }
+    }
+    }
 }
